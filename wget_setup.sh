d3d6a56a59a897e6672c504bdc14a04c99d876bc
Ynes <yangyangteng@gmail.com>
2019-08-18 15:34:32 +0100

    new

--- wget_setup.sh
+++ wget_setup.sh
@@ -5,7 +5,7 @@ apt-get install -y gnutls-dev
 apt-get install -y build-essential
 apt-get install -y pkg-config
 apt-get install -y curl
-curl -O https://ftp.gnu.org/gnu/wget/wget-1.17.tar.gz
+curl https://ftp.gnu.org/gnu/wget/wget-1.17.tar.gz
 tar xvzf wget-1.17.tar.gz
 rm wget-1.17.tar.gz
 cd wget-1.17
