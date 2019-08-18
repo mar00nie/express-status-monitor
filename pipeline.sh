2815302af09961e5c44282c66a5c80ab853c77a3
Ynes <yangyangteng@gmail.com>
2019-08-18 15:19:40 +0100

    new

--- pipeline.sh
+++ pipeline.sh
@@ -4,8 +4,8 @@ fly -t node-app sp -c express-status-monitor/job-node.yml -p build-pipeline
 # Unpause pipeline
 fly -t node-app up -p build-pipeline
 
-# Trigger pipeline jobs
+# Trigger job
 fly -t node-app tj -j build-pipeline/run-pipeline -w
 
-# List of builds
+# Show list of builds
 fly -t node-app builds
