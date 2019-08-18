26bfe317bbac331af97ef5fcbc702480a40fd454
Ynes <yangyangteng@gmail.com>
2019-08-18 01:38:49 +0100

    new

--- wget_setup.sh
+++ wget_setup.sh
@@ -1,6 +1,6 @@
  #!/usr/bin/bash
 
-# Download and install wget-1.17
+# Download and install Wget-1.17
 apt-get install -y gnutls-dev
 apt-get install -y build-essential
 apt-get install -y pkg-config
